88bb3011cec3310fe42e72efea33b158d88f409f
P Punvesh <140485381+Punvesh@users.noreply.github.com>
2025-08-03 14:38:58 +0530

    Time: 116 ms (9.23%), Space: 44 MB (49.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
-                return false;
-            left++;
-            right--;
+    //Declear an stringBuilder object cleaned Because it will
+    //modify the same object without creating an new object for every modification.
+      StringBuilder cleaned = new StringBuilder();
+     //convert the string into character array for an easy 
+     //removal of special characters from String. 
+      for(char c : s.toCharArray()){
+     //from the character it will append and convert to lowerCase only if charcter is an letter  or digit.   
+        if(Character.isLetterOrDigit(c)){
+            //it will append to the cleaned object
+            cleaned.append(Character.toLowerCase(c));
         }
-        return true;
+      }  
+      //convert the charcter array to string for the comparison 
+      String forword =  cleaned.toString();
+      //convert the cleaned object to reverse and  to string
+      String reverse =  cleaned.reverse().toString();
+      // if both forward and reverse if equal it will return "True"
+      return forword.equals(reverse);
     }
 }
