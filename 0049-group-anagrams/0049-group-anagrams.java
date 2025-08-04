6c3d17907418a6c7c4dfb3811ac2e0388c5266b6
P Punvesh <140485381+Punvesh@users.noreply.github.com>
2025-08-04 17:25:40 +0530

    Time: 20 ms (14.87%), Space: 48.3 MB (27.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -4,18 +4,25 @@ class Solution {
         
         for (String str : strs) {
             // Create frequency-based key
+            // for eat e -> count[101-97] -> count[4]++
+            //         a -> count[97-97] -> count[0]++
+            //         t -> count[116-97] -> count[19]++
             int[] count = new int[26];
             for (char c : str.toCharArray()) {
                 count[c - 'a']++;
             }
             
             // Convert frequency array to string key
+            // count[0,4,19] -> 1#0#0#0#1#0#0#0#0#0#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0#
+            // 1 at index 0,4,19
+
             StringBuilder key = new StringBuilder();
             for (int i = 0; i < 26; i++) {
                 key.append(count[i]).append("#");
             }
             
             // Group by frequency key
+            //("1#0#0#0#1#0#0#0#0#0#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0#" : ["eat"])
             map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(str);
         }
         
