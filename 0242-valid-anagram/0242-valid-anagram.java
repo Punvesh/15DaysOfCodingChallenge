79a0bfc30e45be1ae78e0fe665eddebc05ac8294
P Punvesh <140485381+Punvesh@users.noreply.github.com>
2025-08-04 12:27:54 +0530

    Time: 7 ms (33.78%), Space: 44.6 MB (50.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // Quick check: if lengths differ, can't be anagrams.
         if (s.length() != t.length())
             return false;
-
+        // Convert to lowercase to avoid case mismatches.
         s = s.toLowerCase();
         t = t.toLowerCase();
-
+        // Change strings to char arrays for sorting and comparison.
         char[] sarray = s.toCharArray();
         char[] tarray = t.toCharArray();
-
+        // Sort both arrays so we can compare them easily.
         Arrays.sort(sarray);
         Arrays.sort(tarray);
-        
+        // If sorted arrays match, they're anagrams!.
         return Arrays.equals(sarray, tarray);
-
     }
-
 }
