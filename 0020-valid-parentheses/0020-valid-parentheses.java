db1c75db752518aafaa3dd28ad88fe3d65ba0ced
P Punvesh <140485381+Punvesh@users.noreply.github.com>
2025-08-05 16:16:24 +0530

    Time: 2 ms (96.67%), Space: 41.9 MB (31.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') stack.push(')');
-            else if (c == '{') stack.push('}');
-            else if (c == '[') stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c) return false;
+        //convert string to character array 
+        for (char ch : s.toCharArray()) {
+            //if c
+            if (ch == '(') stack.push(')');
+            else if (ch == '{') stack.push('}');
+            else if (ch == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != ch) return false;
         }
         return stack.isEmpty();
     }
