05b4b9af117000ac0cefeb1f6af86fa74fed4679
P Punvesh <140485381+Punvesh@users.noreply.github.com>
2025-08-05 16:24:44 +0530

    Time: 2 ms (96.67%), Space: 41.7 MB (65.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,12 +3,15 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         //convert string to character array 
         for (char ch : s.toCharArray()) {
-            //if c
+            // Push expected closing brackets; check and match when a closing bracket appears.
             if (ch == '(') stack.push(')');
             else if (ch == '{') stack.push('}');
             else if (ch == '[') stack.push(']');
+            // stack.pop() != ch will check if '}' in stack it will pop() it and check ch is not equal.
+            // pop()=} ch = } -> }!=} -> false then stack become empty.
             else if (stack.isEmpty() || stack.pop() != ch) return false;
         }
+        // it will return true if stack is empty.
         return stack.isEmpty();
     }
 }
